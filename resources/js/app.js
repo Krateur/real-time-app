3ad6e198ec29e130a09316963c89cccd62724274
krateur <gjacksongomez@gmail.com>
2019-04-03 18:40:29 +0000

    Feature: Helper Class and Functions

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 
 Vue.use(Vuetify);
+import  User from './Helpers/User'
+Window.User = User;
+
 
 Vue.component('AppHome', require('./components/AppHome').default);
 import router from './Router/router'
